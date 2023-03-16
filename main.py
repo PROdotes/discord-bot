cd7435030a4c0412c104fed267e4b72e0d6f5d03
PROdotes <nguscic@gmail.com>
2023-03-16 14:15:27 +0100

    button

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
+from discord.ext import commands
+from discord.ui import Button, View
 import json
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+GUILD = discord.Object(id=164443857163911168)
+
 
 # Load the user dictionary from file
 try:
@@ -12,29 +15,34 @@ except FileNotFoundError:
     users = {}
 
 
-# report when loaded
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Bot logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync(guild=GUILD)
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
 
-# listen to messages
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    # if someone mentions goat, tell them how many times they did it and save the new vaule
-    if "goat" in message.content.lower():
-        author_id = str(message.author.id)
-        users[author_id] = users.get(author_id, 0) + 1
-        await message.channel.send(f"Bleat! {message.author.name}, You bleated {users[author_id]} times.")
+@bot.tree.command(name='goat', description='fancy goat stuff', guild=GUILD)
+async def goat(interaction: discord.Interaction):
+    response = f"Congrats on sending your command, {interaction.user.mention}!"
+    view = View()
+    button = Button(label='Button!')
+    async def button_reaction(interaction):
+        id = str(interaction.user.mention)[1:]
+        users[id] = users.get(id,0)+1
         with open("users.txt", "w") as file:
             json.dump(users, file)
+        await interaction.response.edit_message(content= f'you have clicked the button {users[id]} times.', view=None)
+    button.callback = button_reaction
+    view.add_item(button)
+    await interaction.response.send_message(response, ephemeral=True, view=view)
 
 
 # read the token from the key file, so it's not in the git
 with open("key.txt") as file:
     TOKEN = file.read().strip()
     print(f'Your token is: {TOKEN}')
-    client.run(TOKEN)
+    bot.run(TOKEN)
